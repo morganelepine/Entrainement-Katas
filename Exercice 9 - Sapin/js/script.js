279363efe3a165803b7995beff94dfae24a053ba
morganelepine <114992081+morganelepine@users.noreply.github.com>
2022-12-08 16:02:23 +0100

    sapin

--- Exercice 9 - Sapin/js/script.js
+++ Exercice 9 - Sapin/js/script.js
@@ -1,15 +1,33 @@
 //-------------------- ETAPE 1
 
+//-----V1
 
 function sapin(etages){
+    let espace = " "
+    let star = ["+ \n"]
+        star.unshift(espace.repeat(etages+2))
+    let branch = ["/", "*", "\\", "\n"]
+        branch.unshift(espace.repeat(etages+1))
+    let branchRight = ["\\", "\n"]
+    let leaf = "**"
+    let christmasTree = [star.join("").toString(), branch.join("").toString()] //join = supprimer les virgules
+    
+    for (let i=etages; i>0; i--){
+        let espace = " "
+        let branchLeft = ["/", "*"]
+        branchLeft.unshift(espace.repeat(i))
+        branchRight.unshift(leaf)
+        christmasTree.push(branchLeft.join("").toString(), branchRight.join("").toString())
+    }
 
+    return christmasTree.join("")
 }
 
-console.log(sapin(1))
-console.log(sapin(3))
-console.log(sapin(3))
+console.log(sapin(5))
 
-document.getElementById("p").innerHTML = sapin(1)
+//document.getElementById("p").innerHTML = sapin(5)
+
+
 
 
 
