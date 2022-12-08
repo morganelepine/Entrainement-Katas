e7b2f43152ac229dd1481e6dc0aed6ab5d20cc8d
morganelepine <114992081+morganelepine@users.noreply.github.com>
2022-12-08 09:54:28 +0100

    sapin

---
+++ Exercice 9 - Sapin/js/script.js
@@ -0,0 +1,27 @@
+//-------------------- ETAPE 1
+
+
+function sapin(etages){
+
+}
+
+console.log(sapin(1))
+console.log(sapin(3))
+console.log(sapin(3))
+
+document.getElementById("p").innerHTML = sapin(1)
+
+
+
+
+/*---------------------Action au clic sur un bouton
+
+document.getElementById("bouton").onclick = function () {
+    combinationOK();
+};
+
+OU
+
+let btn = document.querySelector("input");
+btn.addEventListener("click", combinationOK);
+*/
